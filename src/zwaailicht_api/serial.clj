deb18334c3cfa3ac5a6f3de1516c2233ca959671
Tjisse van der Wal <tjissevdwal@gmail.com>
2016-12-11 14:49:55 +0100

    Fix errors

--- src/zwaailicht_api/serial.clj
+++ src/zwaailicht_api/serial.clj
@@ -7,16 +7,19 @@
 (defn connect! [port-id baud-rate]
   (->>
     (serial/open port-id :baud-rate baud-rate)
-    (println "Serial port connected")
-    (swap! state assoc :conn)))
+    (swap! state assoc :conn))
+  (println "Serial port connected"))
+
+(defn disconnect! [serial-conn]
+  (serial/close! serial-conn)
+  (swap! state assoc :conn nil))
 
 (defn- execute-command! [command serial-conn]
-  (serial/write serial-conn (.getBytes (str command \newline)))
-  nil)
+  (serial/write serial-conn (.getBytes (str command \newline))))
 
 (defn set-state! [new-state]
   (let [serial-conn (get @state :conn)]
-    (if (contains? ["on" "off"] state)
+    (if (.contains ["on" "off"] new-state)
       ((execute-command! new-state serial-conn)
        (reset! state new-state))
-      (:invalid-state))))
+      (::invalid-state))))
